5e2226129438e0e33b7540dba70fec44829b69a4
jhojanmoncaleano <jhojans.moncaleanor@ecci.edu.co>
2022-02-10 21:46:50 -0500

    posible final

--- js/main.js
+++ js/main.js
@@ -145,3 +145,15 @@ ScrollReveal().reveal(".footer", { delay: 450 });
 
 
 
+document.addEventListener("DOMContentLoaded", ()=>{
+    const elementosCarousel= document.querySelectorAll(".carousel");
+    M.Carousel.init(elementosCarousel,{
+        duration:150,
+        dist:-80,
+        shift:5,
+        padding:5,
+        numVisible:5,
+        indicators:true,
+        noWrap:false
+    });
+});
